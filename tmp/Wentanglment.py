7ea683420adb2998afcb3ae870ad540507fe1350
sp1k3 <scjie7894@gmail.com>
2024-04-18 12:45:32 +0800

    modified:   tmp/Wentanglment.py

--- tmp/Wentanglment.py
+++ tmp/Wentanglment.py
@@ -2,46 +2,40 @@ from pyqpanda import *
 import numpy as np
 from scipy.optimize import minimize
 
-def question1(n_qubits_):
-    '''
-    :rtype: Dict[str:float]
-    '''
+def question1(n_qubits):
     qvm = CPUQVM()
     qvm.init_qvm()
-    qubits = qvm.qAlloc_many(n_qubits_)
+    qubits = qvm.qAlloc_many(n_qubits)
     cbits = qvm.cAlloc_many(1)
     result = {}
 
-    x0 = np.random.rand(n_qubits_-2)
+    x0 = np.random.rand(n_qubits-2)
     
     def fun(para):
-
         prog = QProg()
 
         prog << RY(qubits[0], para[0])
-        for i in range(n_qubits_-3):
+        for i in range(n_qubits-3):
             prog << RY(qubits[i+1], para[i+1]).control(qubits[i])
 
-        prog << H(qubits[n_qubits_-2]).control(qubits[n_qubits_-3])
-        for i in range(n_qubits_-1):
-            prog << CNOT(qubits[n_qubits_-2-i],qubits[n_qubits_-1-i])
+        prog << H(qubits[n_qubits-2]).control(qubits[n_qubits-3])
+        for i in range(n_qubits-1):
+            prog << CNOT(qubits[n_qubits-2-i],qubits[n_qubits-1-i])
 
         prog << X(qubits[0])
 
-        #print(prog)
         prob = qvm.prob_run_dict(prog, qubits, -1)
-        #print(prob)
         prob = list(prob.values())
 
-        tar = 1/n_qubits_
+        tar = 1/n_qubits
         loss = 0
         j = 1
-        for i in range(2 ** n_qubits_):
+        for i in range(2 ** n_qubits):
             if i == j:
-                loss += abs(prob[i] - tar)
+                loss += abs(prob[i] - tar) ** 2
                 j *= 2
             else:
-                loss += prob[i]
+                loss += prob[i] ** 2
         return loss
     
     res = minimize(fun,x0,method='COBYLA')
@@ -49,21 +43,30 @@ def question1(n_qubits_):
 
     prog = QProg()
     prog << RY(qubits[0], para[0])
-    for i in range(n_qubits_-3):
+    for i in range(n_qubits-3):
         prog << RY(qubits[i+1], para[i+1]).control(qubits[i])
-    prog << H(qubits[n_qubits_-2]).control(qubits[n_qubits_-3])
-    for i in range(n_qubits_-1):
-        prog << CNOT(qubits[n_qubits_-2-i],qubits[n_qubits_-1-i])
+    prog << H(qubits[n_qubits-2]).control(qubits[n_qubits-3])
+    for i in range(n_qubits-1):
+        prog << CNOT(qubits[n_qubits-2-i],qubits[n_qubits-1-i])
     prog << X(qubits[0])
 
     prob = qvm.prob_run_dict(prog, qubits, -1)
-    #print(prob)
-    filtered_dict = {key: value for key, value in prob.items() if key.count('1') == 1}
-    print(filtered_dict)
+    result = {key: value for key, value in prob.items() if key.count('1') == 1}
 
-    return filtered_dict
+    print(result)
+    return result
 
 
+def question2(n_qubits):
+    qvm = CPUQVM()
+    qvm.init_qvm()
+    qubits = qvm.qAlloc_many(n_qubits)
+    cbits = qvm.cAlloc_many(1)
+    result = {}
+
+
+
+    return result
 
 if __name__ == '__main__':
-    question1(4)
+    question1(5)
